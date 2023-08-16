4fbff3730e0e0cb8e236e735df74fea0e7fbbdf0
13Mudit <13mudit@gmail.com>
2023-08-16 22:45:57 +0530

    age and gender values from user collection

--- main.py
+++ main.py
@@ -106,12 +106,12 @@ async def generate(query: Query):
     on_sale_products = []
     get_on_sale_query =  {
     "$and": [
-                {"age": {"$regex": f".*{user_preference_from_prompt['age']}.*" if user_preference_from_prompt['age'] is not None else ".*"}},
+                {"age": {"$regex": f".*{user_preference['age']}.*" if user_preference['age'] is not None else ".*"}},
                 {"season": {"$regex": f".*{user_preference_from_prompt['season']}.*" if user_preference_from_prompt['season'] is not None else ".*"}},
                 {
                     "$or": [{"type": {"$regex": f".*{t}.*"}} for t in user_preference_from_prompt['type']] if user_preference_from_prompt['type'] else [{"type": {"$regex": ".*"}}]
                 },
-                {"gender": user_preference_from_prompt['gender'] if user_preference_from_prompt['gender'] else {"$exists": True}},
+                {"gender": user_preference['gender'] if user_preference['gender'] else {"$exists": True}},
                 {
                     "$or": [{"colour": {"$regex": f".*{c}.*"}} for c in user_preference_from_prompt['colour']] if user_preference_from_prompt['colour'] else [{"colour": {"$regex": ".*"}}]
                 }
